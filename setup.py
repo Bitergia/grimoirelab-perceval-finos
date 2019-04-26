a2bb875048a0140df185c7cbb5a4497e4b0be0b3
Santiago Dueñas <sduenas@bitergia.com>
2019-04-26 12:59:46 -0400

    Update Perceval requirement to 0.12.12 or later

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ setup(name="perceval-finos",
       install_requires=[
           'requests>=2.7.0',
           'grimoirelab-toolkit>=0.1.9',
-          'perceval>=0.12'
+          'perceval>=0.12.12'
       ],
       cmdclass=cmdclass,
       zip_safe=False)
